42493e88c7667c7fa7c40b445041a2ccfafeb6fd
hytczhangqiangqiang <1124888277@qq.com>
2014-09-11 16:51:57 +0800

    2014-9-11
    web chat聊天好友列表在线离线

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	include_once "include/ez_sql_core.php";
 	include_once "include/ez_sql_mysql.php";
+	session_start();
 	$userid =isset($_POST["userid"])?$_POST["userid"]:"";
 	$userpwd =isset($_POST["userpwd"])?$_POST["userpwd"]:"";
 	if($userid != "" && $userpwd != ""){
@@ -22,11 +23,51 @@
 	$curid =isset($_SESSION["wodeid"])?$_SESSION["wodeid"]:"";
 	$curnicheng =isset($_SESSION["wodenicheng"])?$_SESSION["wodenicheng"]:"";
 	if ($curid == "") {
-		header("location:login.php?error=needlogin");
-		die();
+		//header("location:login.php?error=needlogin");
+		//die();
 	}else{
 		echo "welcome" . $curnicheng;
 	}
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>无标题</title>
+</head>
+<body>
+	<a class="logout" href="login.php?logout=yes">logout</a>
+	<div id="friendList">
+		<ul id="onlineFriendList">
+			<?php
+				$db = new ezSQL_mysql();
+				$res=$db->get_results("select * from userinfo,friendsinfo where userinfo.id=friendsinfo.friendid and friendsinfo.userid=$curid");
+				$onlineHtml="";
+				$offlineHtml="";
+				if ($res) {
+					foreach ($res as $friend) {
+						$headimgurl=$friend->userHeadImage;
+						$friendnicheng=$friend->friendNoteName;
+						if ($friend->userState=="online") {
+							$onlineHtml .="<li class='friendLi'><img src='$headimgurl' class='friendImg' /><a class='friendnicheng'>$friendnicheng</a></li>";
+						}else{
+							$offlineHtml .="<li class='friendLi'><img src='$headimgurl' class='friendImg offlinePic' /><a class='friendnicheng'>$friendnicheng</a></li>";
+						}
+					}
+				}
+				echo $onlineHtml;
+			?>
+		</ul>
+		<ul id="offlineFriendList">
+			<?php
+				echo $offlineHtml;
+			?>
+		</ul>
+	</div>
+	<div id="chatArea"></div>
 
 	
-?>
+</body>
+</html>
+
--- login.php
+++ login.php
@@ -1,5 +1,6 @@
-
-
+<?php
+	session_start();
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
@@ -14,8 +15,13 @@
 	</form>
 <?php
 	$info = isset($_GET["error"])?$_GET["error"]:"";
+	$logout = isset($_GET["logout"])?$_GET["logout"]:"";
 	if ($info == "wrongpwd") {
 		echo "<script>tan();</script>";
+	}
+	if ($logout =="yes") {
+		unset($_SESSION["wodeid"]);
+		unset($_SESSION["wodenicheng"]);
 	}
 ?>
 </body>
